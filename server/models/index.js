46654ad4ef98ca3e82c4112ed34bcedb0e25d3c8
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-12 15:41:57 -0400

    added user model

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// Initializes a connection based on environment
 if (config.use_env_variable) {
   let sequelize = new Sequelize(process.env[config.use_env_variable])
 } else {
   let sequelize = new Sequelize(config.database, config.username, config.password, config); 
 }
 
+// Associates models from js files in models directory
 fs
   .readdirSync(__dirname)
   .filter(file => {
