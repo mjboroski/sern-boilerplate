3dde209f7e472266e14f1519b7922db68d266211
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-16 13:40:32 -0400

    webpack config

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,23 @@
-// TODO: add user model here
+const bcrypt = require('bcrypt-nodejs');
+
+module.exports = (sequelize, DataTypes) => {
+  let User = sequelize.define('user', {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+      instanceMethods: {
+        generateHash: (password) => {
+          return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+        },
+        validPassword: (password) => {
+          return bcrypt.compareSync(password, this.password);
+        },
+      }
+    });
+
+  User.hook('beforeCreate', (user, options) => {
+    user.password = user.generateHash(user.password);
+  });
+
+  return User;
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,33 @@
-const webpack = require('webpack');
 const path = require('path');
+const SRC = path.join(__dirname, '/client');
+const DIST = path.join(__dirname, '/server/dist');
+
+module.exports = {
+  entry: `${SRC}/main.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+};
