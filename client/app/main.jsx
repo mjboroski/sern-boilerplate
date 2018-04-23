a6b021044d09b88c665104b40c0b51cede6056ff
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-04-23 18:53:46 -0400

    setting up react router

--- client/app/main.jsx
+++ client/app/main.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, Router, IndexRoute, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const FourOhFour = () => <h1>404</h1>;
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+render(<App />, document.getElementById('app'));
